f597a6de67cd7afdcd40f04d2449a77b95430be7
AliAlsed <alialsed1996@yahoo.com>
2017-10-31 21:49:29 +0300

    not work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  message:any;
+  ciphertext:any;
+  plaintext:any;
+ 
+  encryptdata(a, b){
+    var A=a.toString(16)+ "";
+    var B=b.toString(16) +"";
+      var res = "",
+         i = A.length,
+         j = B.length;
+     while (i-- > 0 && j-- > 0) {
+          res = (parseInt(A.charAt(i), 16) ^ parseInt(b.charAt(j), 16)).toString(16) + res;
+     }
+     return res;
+  }
+  encrypt(){
+    this.ciphertext=this.encryptdata(this.message,"36be452d4f4e6af0");
+    console.log(this.ciphertext);
+  }
+  Decryptdata(a,b){
+    var A=a.toString(16)+ " ";
+    var B=b.toString(16) +" ";
+      var res = "",
+         i = A.length,
+         j = B.length;
+     while (i-- > 0 && j-- > 0) {
+          res = (parseInt(A.charAt(i), 16) ^ parseInt(b.charAt(j), 16)).toString(16);
+     }
+     return res.toString();
+  }
+  Decrypt(){
+    this.plaintext=this.encryptdata(this.ciphertext,"36be452d4f4e6af0");
+    console.log(this.plaintext);
+  }
+  
+
 }
